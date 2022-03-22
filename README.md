3ef7b037f390991d83f7184a870fe4522443a2b0
IlFed <ilya.fedoseev.95@gmail.com>
2022-03-22 15:45:07 +0300

    final

---
+++ README.md
@@ -0,0 +1,5 @@
+### use vue 3, nodejs - v16.13.0
+
+### To run it, download to the desired directory with the command "git clone https://github.com/IlFed/test-alef"
+### Enter the command "npm i"
+### Enter the command "npm run dev"
---
+++ src/components/VButton.vue
@@ -0,0 +1,86 @@
+<template>
+  <button class="btn" :class="buttonClass">
+    <div class="plus">
+      <div class="union"></div>
+    </div>
+    <slot/>
+  </button>
+</template>
+
+<script setup>
+import { defineProps, computed, toRefs } from 'vue';
+
+const props = defineProps({
+  type: {
+    type: String,
+    default: 'filled',
+  },
+});
+const { type } = toRefs(props);
+
+const buttonClass = computed(() => `btn--${type.value}`);
+</script>
+
+<style lang="scss" scoped>
+
+.btn {
+  display: flex;
+  align-items: center;
+  height: 44px;
+  padding: 10px 20px;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 24px;
+  text-align: center;
+  border-radius: 100px;
+  cursor: pointer;
+  border: 2px solid #FFFFFF;
+  background-color: transparent;
+  & :first-child {
+    display: flex;
+    align-items: center;
+    & .icon:hover{
+      fill: #7acef8;
+    }
+  }
+  &--filled {
+    background-color: #01A7FD;
+    border-color: #01A7FD;
+    color: #FFFFFF;
+    &:hover {
+      background-color: #7acef8;
+      border-color: #7acef8;
+    }
+  }
+  &--outlined {
+    color: #01A7FD;
+    border-color: #01A7FD;
+    .plus{
+      margin-right: 8px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 24px;
+      height: 24px;
+      position: relative;
+      .union {
+        background-color: #01A7FD;
+        height: 24px;
+        width: 2.3px;
+        border-radius: 138.148px;
+      }
+      .union:after {
+        content: "";
+        position: absolute;
+        height: 2.3px;
+        width: 24px;
+        left: 0px;
+        top: 11px;
+        background: #01A7FD;
+        border-radius: 138.148px;
+      }
+    }
+  }
+}
+</style>
